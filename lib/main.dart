816f9cf8775c1de65c883f43c79c872f4a563b2b
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-12-18 16:51:19 +0700

    setup unutk supabasenya

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,17 @@ import 'package:belajar_upload/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:belajar_upload/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
+  await Supabase.initialize(
+    url: 'https://PROJECT_ID.supabase.co',
+    anonKey: 'ANON_PUBLIC_KEY',
   );
   runApp(const MyApp());
 }
