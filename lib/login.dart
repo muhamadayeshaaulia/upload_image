5436b774ee2b061eacefc73d1f03c8e78ee9ee9e
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-12-18 15:51:11 +0700

    setup ngebuat file login dashboard dan main nya agar bisa konek dengan firebase optionnya

---
+++ lib/login.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyLogin extends StatelessWidget {
+  const MyLogin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text('Welcome to the Login Page'),
+      ),
+    );
+  }
+}
