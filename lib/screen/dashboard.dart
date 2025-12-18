4efd9726d5f349627cec611219e5392808c748f2
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-12-18 17:48:42 +0700

    membuat function dashboard untuk membuat upload delete image jadi gambar memiliki uid yg sama dengan uid di firebase sesuai dengan user yg login

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -12,7 +12,48 @@ class MyDashboard extends StatefulWidget {
 }
 
 class _MyDashboardState extends State<MyDashboard> {
+  final supabase = Supabase.instance.client;
+  final picker = ImagePicker();
 
+  List<String> files = [];
+
+  String get uid => FirebaseAuth.instance.currentUser!.uid;
+
+  @override
+  void initState() {
+    super.initState();
+    loadImages();
+  }
+
+  Future<void> loadImages() async {
+    final res = await supabase.storage.from('images').list(path: uid);
+    setState(() {
+      files = res.map((e) => e.name).toList();
+    });
+  }
+
+  Future<void> uploadImage() async {
+    final picked = await picker.pickImage(source: ImageSource.gallery);
+    if (picked == null) return;
+
+    final file = File(picked.path);
+    final fileName = '${DateTime.now().millisecondsSinceEpoch}.jpg';
+
+    await supabase.storage
+        .from('images')
+        .upload('$uid/$fileName', file);
+
+    loadImages();
+  }
+
+  Future<void> deleteImage(String name) async {
+    await supabase.storage.from('images').remove(['$uid/$name']);
+    loadImages();
+  }
+
+  String getUrl(String name) {
+    return supabase.storage.from('images').getPublicUrl('$uid/$name');
+  }
 
   @override
   Widget build(BuildContext context) {
