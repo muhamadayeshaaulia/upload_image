4114388ac530ef79e4164b260cd8efa7244a82dc
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-12-18 16:52:33 +0700

    menambahkan url supabase dan Api publik supabase kedalam main yg sudah ada

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,8 @@ void main() async {
   );
   
   await Supabase.initialize(
-    url: 'https://PROJECT_ID.supabase.co',
-    anonKey: 'ANON_PUBLIC_KEY',
+    url: 'https://wfboqhhjbufeidkqppxg.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmYm9xaGhqYnVmZWlka3FwcHhnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjYwNDY1MzMsImV4cCI6MjA4MTYyMjUzM30.BN7nBSgrHed7GJ0Yb2Ze8mklTJ4Qo4TihZXTE6smuR0',
   );
   runApp(const MyApp());
 }
