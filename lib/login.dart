a713e5de075c0f4c3cc9134333d9bbebcd5e7d57
Muhamad Ayesha Aulia <shaxyyy.03@gmail.com>
2025-12-18 16:04:49 +0700

    membuat myloginstate

--- lib/login.dart
+++ lib/login.dart
@@ -3,7 +3,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class MyLogin extends StatelessWidget {
   const MyLogin({super.key});
+  @override
+  State<MyLogin> createState() => _MyLoginState();
+}
 
+class _MyLoginState extends State<MyLogin> {
+  final emailCtrl = TextEditingController();
+  final passCtrl = TextEditingController();
+  bool _isLoading = false;
+  String? _error;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -12,4 +21,3 @@ class MyLogin extends StatelessWidget {
       ),
     );
   }
-}
